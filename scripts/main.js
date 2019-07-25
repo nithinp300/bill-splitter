35aee5db4891bd90fb85277e6a314e3d95a841f1
Nithin Pingili <nithinp300@gmail.com>
2019-07-25 18:32:30 -0500

    got the form and buttoms done

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function split() {
+  var total = parseInt(document.querySelector('#total').value);
+  // var people = parseInt(document.querySelector('#people').value);
+  // var tipPercent = document.querySelector('#tipPercent').value;
+  // var tipCustom = document.querySelector('#tipCustom').value;
+  console.log(total);
+  // console.log(people);
+  // console.log(tipPercent);
+  // console.log(tipCustom);
+}
