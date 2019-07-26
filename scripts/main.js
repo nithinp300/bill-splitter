2b2c49e4e760c21208ccbec4bb1cbd1ea9b37968
Nithin Pingili <nithinp300@gmail.com>
2019-07-26 17:51:15 -0500

    split the bill with and without percent or custom tip. reset the form. added tip input show/hide option

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,51 @@
+document.querySelector('#splitBtn').addEventListener('click', (e) => {
+    e.preventDefault();
+    split();
+})
+
+document.querySelector('#addTip').addEventListener('click', (e) => {
+  e.preventDefault();
+  showTipInput();
+})
+
+document.querySelector('#resetBtn').addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('form').reset();
+  document.querySelector('#perPerson').innerHTML = "$0"
+})
+
 function split() {
   var total = parseInt(document.querySelector('#total').value);
-  // var people = parseInt(document.querySelector('#people').value);
-  // var tipPercent = document.querySelector('#tipPercent').value;
-  // var tipCustom = document.querySelector('#tipCustom').value;
-  console.log(total);
-  // console.log(people);
-  // console.log(tipPercent);
-  // console.log(tipCustom);
+  var people = parseInt(document.querySelector('#people').value);
+  var tipPercent = document.querySelector('#tipPercent').value;
+  var tipCustom = document.querySelector('#tipCustom').value;
+  var perPerson = total / people; // default is assuming no tip was given
+  if(tipCustom != "" || tipPercent != "")
+  {
+    // tip was given
+    if(tipCustom == "")
+    {
+      tipPercent = parseInt(tipPercent);
+      perPerson = (total + (tipPercent/100 * total)) / people;
+    }
+    else
+    {
+      tipCustom = parseInt(tipCustom);
+      perPerson = (total + tipCustom) / people;
+    }
+  }
+  document.querySelector('#perPerson').innerHTML = "$"+perPerson.toFixed(2);
+}
+
+function showTipInput()
+{
+  var tipInputs = document.querySelector('#tipInput');
+  if(tipInputs.style.display === "block")
+  {
+    tipInputs.style.display = "none";
+  }
+  else
+  {
+    tipInputs.style.display = "block";
+  }
 }
